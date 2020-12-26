2c3e1e2879e3860db04404a6402056c03f381c72
felipe ospina calle <felipeospina21@hotmail.com>
2020-12-26 17:54:10 -0500

    contact page components

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactInfo from '../components/ContactInfo/ContactInfo';
+import ContactForm from '../components/ContactForm/ContactForm';
+import Layout from '../components/Layout/Layout';
 
 const contacto = () => {
 	return (
-		<div>
-
+		<Layout>
+			<ContactForm />
 			<ContactInfo />
-		</div>
+		</Layout>
 	);
 };
 
