f41db8aefc4e0f57f325309c324bf573af701cc1
felipe ospina calle <felipeospina21@hotmail.com>
2021-09-25 16:48:00 -0500

    add cms categories

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,102 +1,102 @@
 module.exports = {
-	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@felipeospina`,
-		social: [
-			{
-				url: 'https://github.com/felipeospina21',
-				name: 'GitHub'
-			}
-		]
-	},
-	plugins: [
-		{
-			resolve: 'gatsby-plugin-web-font-loader',
-			options: {
-				google: {
-					families: ['Montserrat']
-				}
-			}
-		},
-		`gatsby-plugin-netlify-cms`,
-		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-sass`,
-		'gatsby-plugin-breakpoints',
-		`gatsby-transformer-yaml`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `tags-list`,
-				path: `./content/`
-			}
-		},
-		{
-			resolve: 'gatsby-plugin-react-svg',
-			options: {
-				rule: {
-					include: /assets/ // See below to configure properly
-				}
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `markdown-pages`,
-				path: `${__dirname}/content`
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `markdown-pages`,
-				path: `${__dirname}/content/blog`
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `markdown-pages`,
-				path: `${__dirname}/content/portfolio`
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `blog-images`,
-				path: `${__dirname}/static/img`
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/images`
-			}
-		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-plugin-mdx`,
-			options: {
-				extensions: [`.md`, `.mdx`]
-			}
-		},
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `Portafolio Felipe Ospina`,
-				short_name: `portfolio`,
-				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
-				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-			}
-		}
+  siteMetadata: {
+    title: `Portafolio Felipe Ospina`,
+    description: `Desarrollo web y automatizaciones`,
+    author: `@felipeospina`,
+    social: [
+      {
+        url: "https://github.com/felipeospina21",
+        name: "GitHub",
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Montserrat"],
+        },
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    "gatsby-plugin-breakpoints",
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `tags-list`,
+        path: `./content/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content/portfolio`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog-images`,
+        path: `${__dirname}/static/img`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Portafolio Felipe Ospina`,
+        short_name: `portfolio`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
 
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
-	]
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 };
