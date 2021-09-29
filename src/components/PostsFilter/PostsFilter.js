99f52e3837b5548e3d04ad08e50077232deeabc8
felipe ospina calle <felipeospina21@hotmail.com>
2021-09-29 17:57:47 -0500

    fix poster filter issuefor tags without project

--- src/components/PostsFilter/PostsFilter.js
+++ src/components/PostsFilter/PostsFilter.js
@@ -1,37 +1,39 @@
-import React, { useState } from 'react';
-import TagsFilter from '../TagsFilter/TagsFilter';
-import PostPrevWrap from '../PostPrevWrap/PostPrevWrap';
-import '../PostPreview/PostPreview.scss';
+import React, { useState } from "react";
+import TagsFilter from "../TagsFilter/TagsFilter";
+import PostPrevWrap from "../PostPrevWrap/PostPrevWrap";
+import "../PostPreview/PostPreview.scss";
 
 const PostsFilter = ({ data }) => {
-	const allPosts = [...data.allMdx.edges];
-	const [filteredPosts, setFilteredPosts] = useState([]);
+  const allPosts = [...data.allMdx.edges];
+  const [filteredPosts, setFilteredPosts] = useState(allPosts);
 
-	const filterPostsByCategory = categoryName => {
-		const filterByCategory = allPosts.filter(post =>
-			post.node.frontmatter.tags.includes(categoryName)
-		);
-		setFilteredPosts(filterByCategory);
-	};
+  const filterPostsByCategory = categoryName => {
+    const filterByCategory = allPosts.filter(post =>
+      post.node.frontmatter.tags.includes(categoryName)
+    );
+    setFilteredPosts(filterByCategory);
+  };
 
-	const filterAllPosts = () => {
-		setFilteredPosts(allPosts);
-	};
+  const filterAllPosts = () => {
+    setFilteredPosts(allPosts);
+  };
 
-	return (
-		<>
-			<TagsFilter
-				categories={data.contentYaml.categories}
-				filterAllPosts={filterAllPosts}
-				filterPostsByCategory={filterPostsByCategory}
-			/>
-			{filteredPosts.length < 1 ? (
-				<PostPrevWrap postsArray={allPosts} />
-			) : (
-				<PostPrevWrap postsArray={filteredPosts} />
-			)}
-		</>
-	);
+  return (
+    <>
+      <TagsFilter
+        categories={data.contentYaml.categories}
+        filterAllPosts={filterAllPosts}
+        filterPostsByCategory={filterPostsByCategory}
+      />
+      {filteredPosts.length < 1 ? (
+        <div className='post-preview-wrapper'>
+          <h3>No se encontraron proyectos</h3>
+        </div>
+      ) : (
+        <PostPrevWrap postsArray={filteredPosts} />
+      )}
+    </>
+  );
 };
 
-export default PostsFilter
+export default PostsFilter;
