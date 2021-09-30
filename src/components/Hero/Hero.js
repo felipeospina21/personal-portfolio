6a48312a20dcf90d5b19b7c901ace0f1e37bd069
felipe ospina calle <felipeospina21@hotmail.com>
2021-09-30 17:27:07 -0500

    add animation to Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 import Image from "../image";
 import "./Hero.scss";
@@ -5,20 +6,79 @@ import "./Hero.scss";
 const Hero = () => {
   return (
     <div className='hero-wrapper'>
-      <Image className='hero-img' src='background3.jpg' alt='imagen abstracta de fondo' />
       <div className='hero-container'>
-        <h1>Hola, soy Felipe</h1>
-        <h3>Me especializo en desarrollo web y automatización de procesos.</h3>
+        <AnimatedDiv>
+          <VisibilityTransition>
+            <h1>Felipe Ospina</h1>
+          </VisibilityTransition>
+          <VisibilityTransition>
+            <h3>Front-End Dev</h3>
+          </VisibilityTransition>
+        </AnimatedDiv>
       </div>
-      <a
-        href='http://api.whatsapp.com/send?phone=573148871629'
-        rel='noreferrer'
-        target='_blank'
-        className='hero-btn'>
-        Conversemos
-      </a>
+
+      <div className='hero-container'>
+        <VisibilityTransition>
+          <a
+            href='http://api.whatsapp.com/send?phone=573148871629'
+            rel='noreferrer'
+            target='_blank'
+            // className='hero-btn'
+          >
+            Conversemos
+          </a>
+        </VisibilityTransition>
+      </div>
     </div>
   );
 };
 
 export default Hero;
+
+export const AnimatedDiv = ({ children }) => {
+  const variants = {
+    visible: {
+      transition: {
+        delay: 0.1,
+      },
+    },
+    hidden: {
+      backgroundColor: "#e3a83b00",
+      scale: [1, 2, 2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    },
+  };
+  return (
+    <motion.div
+      variants={variants}
+      initial='visible'
+      animate='hidden'
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeatDelay: 1,
+      }}>
+      {children}
+    </motion.div>
+  );
+};
+
+const VisibilityTransition = ({ children }) => {
+  const variants = {
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 2.2,
+      },
+    },
+    hidden: { opacity: 0 },
+  };
+
+  return (
+    <motion.span variants={variants} initial='hidden' animate='visible'>
+      {children}
+    </motion.span>
+  );
+};
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import Header from "../Header/Header"
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>{children}</main>
       <Footer/>
     </>
